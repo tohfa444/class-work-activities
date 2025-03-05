aea488174ca2da5cd86175681bc66ea6cd2eb6b5
tohfa444 <svoil.flour@gmail.com>
2025-03-05 22:07:32 +0600

    initial commit

---
+++ L2_A1.py
@@ -0,0 +1,17 @@
+#This is a comment
+print("My name is Tohfa")
+
+#print a number
+print(4444444)
+
+#Next line after print
+print("Hello world\n")
+
+#print multiple arguments for printing
+print("Hello", 4)
+
+#End argument of print statement
+print("Welcome to ",end="*")
+
+
+
---
+++ L2_A2.py
@@ -0,0 +1,4 @@
+#User input
+name=input("what's your name ")
+print("\nHello",name,"\nwelcome to codingal")
+
