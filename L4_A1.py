07f278b2c2eecfd16cc90cc85380c1072004b430
tohfa444 <svoil.flour@gmail.com>
2025-03-15 21:51:49 +0600

    initial commit

---
+++ L4_A1.py
@@ -0,0 +1,14 @@
+#storing values
+tree1=4
+tree2=44
+tree3=444
+
+#finding the total of trees
+sum_=tree1+tree2+tree3
+print("the sum of all the three trees is ",sum_)
+
+#findint the average of trees
+average=sum_/3
+print("the average all the trees is ",average)
+
+
