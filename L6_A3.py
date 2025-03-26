23279974f7a357225c496fd58e807e6758744c43
tohfa444 <svoil.flour@gmail.com>
2025-03-26 21:43:57 +0600

    initial commit

---
+++ L6_A3.py
@@ -0,0 +1,16 @@
+height=float(input("enter the height in centimeter: "))
+weight=float(input("enter the weight in kg: "))
+BMI=weight/(height/100)**2
+print("your BMI is ",BMI)
+if BMI<=18.4:
+    print("You are underweight")
+elif BMI<=24.9:
+    print("You are healthy")  
+elif BMI<=29.9:
+    print("You are overweight") 
+elif BMI<=34.9:
+    print("You are severely overweight")
+elif BMI<=39.9:
+    print("You are obese")
+else:
+    print("you are severely obese")
---
+++ L7_A1.py
@@ -0,0 +1,21 @@
+#python program to illustrate the use of "is" identity operator
+x=44
+if (type(x) is int):
+    print("true")
+else:
+    print("false")
+
+x=44.4
+if (type(x) is not float):
+    print("true")
+else:
+    print("false") 
+
+x=34
+y=34
+if (x is y):
+    print("X and Y have same identity")
+
+y=35
+if (x is not y):
+    print("X and Y have differnt identity")
