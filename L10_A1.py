69ee989e22e7e6b9a5626f4bde96bb1ee98c0e73
tohfa444 <svoil.flour@gmail.com>
2025-04-12 21:51:35 +0600

    initial commit

---
+++ L10_A1.py
@@ -0,0 +1,7 @@
+#input an inteeger value
+n=int(input("Enter the number whose sum you want to find"))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+    print("\nsum=",sum)
+
---
+++ L10_A2.py
@@ -0,0 +1,7 @@
+#input a word or sentence
+string=input("Please enter your own string")
+string2=""
+for i in string:
+    string2=i+string2
+print("The original string is ",string)
+print("everse string is ",string2)
