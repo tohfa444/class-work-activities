d72dad698a869e7ff6d8900a73836269414dade5
tohfa444 <svoil.flour@gmail.com>
2025-07-05 21:51:42 +0600

    initial commit

---
+++ L28_A2.py
@@ -0,0 +1,10 @@
+class vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+modelX=vehicle(240,18)
+
+print("Model max speed: ",modelX.max_speed)
+print("Model mileage: ",modelX.mileage)
+
+        
