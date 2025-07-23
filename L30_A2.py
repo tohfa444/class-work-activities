9305e69b7debffb6ab1d2ec151b3d10b6bacd85b
tohfa444 <svoil.flour@gmail.com>
2025-07-23 22:07:23 +0600

    initial commit

--- L30_A2.py
+++ L30_A2.py
@@ -5,3 +5,24 @@ class bird:
 
     def whoisthis(self):
         print("Bird")
+
+    def swin(self):
+        print("Swin faster")
+    
+class penguin(bird):
+
+    def __init__(self):
+
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisthis(self):
+        print("Peanguin")
+
+    def run(self):
+        print("Run faster")
+
+peggy = penguin()
+peggy.whoisthis()
+peggy.swin()
+peggy.run()
