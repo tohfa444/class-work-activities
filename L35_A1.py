96300d4f34b048fa2359f1bb67d87c88dc2e46ef
tohfa444 <svoil.flour@gmail.com>
2025-09-25 16:59:31 +0600

    initial commit

---
+++ L35_A1.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+pygame.init()
+
+SPRITE_COLOR_CHANGE_EVENT = pygame.USERVENT+1
+BACKGROUND_COLOR_CHANGE_EVENT = pygame.USERVENT+2
+
+BLUE = pygame.color("blue")
+LIGHTBLUE = pygame.color("lightblue")
+DARKBLUE = pygame.color("darkblue")
+
+YELLOW = pygame.color("yellow")
+ORANGE = pygame.color("orange")
+MAGENTA = pygame.color("magenta")
+WHITE = pygame.color("white")
