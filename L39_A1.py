e07ee5155454d808f8e09a44674a1b25e4b2c140
tohfa444 <svoil.flour@gmail.com>
2025-10-13 16:53:36 +0600

    initial commit

---
+++ L39_A1.py
@@ -0,0 +1,15 @@
+# from tkinter import *
+
+# root = Tk()
+# root.title("login App")
+# root.geometry('400x400')
+
+# frame = Frame(master=root, height=200, width=360, bg='#d0efff')
+
+# lbl1 = Label(frame, text='Full Name', bg='389503', fg='white',width=12)
+# lbl = Label(frame, text='Email ID', bg='389503', fg='white',width=12)
+# lbl = Label(frame, text='Enter password', bg='389503', fg='white',width=12)
+
+# name_entry = Entry(frame)
+# email_entry = Entry(frame)
+# pass_entry = Entry(frame, show='*')
